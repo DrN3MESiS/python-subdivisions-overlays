c0db3396b15c8858f171a10ee2108f8f0a751a64
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-03-19 14:47:46 -0600

    Last changes

--- Reader.py
+++ Reader.py
@@ -1,3 +1,58 @@
+from Segmento import Segmento
+from Punto import Punto
 
-def obtenerSegmentos(names=[]):
-    pass
+
+def getSegments(names=[]):
+    if len(names) == 0:
+        return BaseException
+    segments = []
+
+    for name in names:
+        vertexFile = name+".ver"
+        edgeFile = name+".ari"
+        segments.append(readContentFromFiles(vertexFile, edgeFile))
+
+
+def readContentFromFiles(vertFile, edgeFile):
+    points = []
+    edges = []
+    try:
+        points = __readPoints__(vertFile)
+    except Exception as e:
+        print(e)
+
+    print(points)
+
+    try:
+        edges = __convertToSegments__(edgeFile, points)
+    except Exception as e:
+        print(e)
+
+
+def __readPoints__(file):
+    points = []
+    f = open(file, "r")
+    if f.mode != "r":
+        return Exception
+    content = f.readlines()
+    content = content[4:]
+    for line in content:
+        data = " ".join(line.split())
+        data = data.split(" ")
+        x = int(data[1])
+        y = int(data[2])
+        points.append(Punto(x, y))
+    return points
+
+
+def __convertToSegments__(file, points):
+    edges = []
+    f = open(file, "r")
+    if f.mode != "r":
+        return Exception
+    content = f.readlines()
+    content = content[4:]
+    for line in content:
+        data = " ".join(line.split())
+        data = data.split(" ")
+        print(data)
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from algoritmo import AlgoritmoBarrido
 from Punto import Punto
 from Segmento import Segmento
-from Reader import obtenerSegmentos
+from Reader import getSegments
 
 
 def main():
+    names = ["ejemplo_01/layer01", "ejemplo_01/layer02"]
+    segmentos = getSegments(names)
 
-    s1 = Segmento(Punto(10, 10), Punto(0, 0))
-    s2 = Segmento(Punto(10, 0), Punto(0, 10))
+    # s1 = Segmento(Punto(10, 10), Punto(0, 0))
+    # s2 = Segmento(Punto(10, 0), Punto(0, 10))
 
-    segmentos = [s1, s2]
+    # segmentos = [s1, s2]
 
-    for s in segmentos:
-        print(s)
+    # for s in segmentos:
+    #     print(s)
 
-    barr = AlgoritmoBarrido(segmentos)
-    barr.barrer()
-    print(barr.R)
+    # barr = AlgoritmoBarrido(segmentos)
+    # barr.barrer()
+    # print(barr.R)
 
 
 if __name__ == "__main__":
