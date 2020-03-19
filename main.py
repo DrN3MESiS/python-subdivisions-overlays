00c01e2d27e6431c8078f077479978a53f6e2316
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-03-19 13:24:00 -0600

    UPDATE: Overall code redo; FEATURE: Examples

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from pprint import pprint
 from algoritmo import AlgoritmoBarrido
 from Punto import Punto
 from Segmento import Segmento
 
-
 def main():
 
-    s1 = Segmento(Punto(0, 0), Punto(10, 10))
-    s2 = Segmento(Punto(10, 0), Punto(0, 10))
-    segmentos = [s1, s2]
-    for s in segmentos:
-        print(s)
-    barr = AlgoritmoBarrido(segmentos)
+  s1 = Segmento(Punto(10,10), Punto(0,0))
+  s2 = Segmento(Punto(10,0), Punto(0,10))
 
-    barr.barrer()
-#  for e in barr:
-#    print(e)
-    print(barr.R)
+  segmentos = [s1,s2]
 
+  for s in segmentos:
+      print(s)
 
-if __name__ == "__main__":
+  barr = AlgoritmoBarrido(segmentos)
+  barr.barrer()
+  print(barr.R)
+
+if __name__=="__main__":
     main()
+
+
+
