b16a1852ba4a1d358c23449446609db20bd69ae2
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-03-21 11:56:37 -0600

    Working Reader

--- Reader.py
+++ Reader.py
@@ -2,7 +2,7 @@ from Segmento import Segmento
 from Punto import Punto
 
 
-def getSegments(names=[]):
+def getSegments(names=[], points=[]):
     if len(names) == 0:
         return BaseException
     segments = []
@@ -10,27 +10,30 @@ def getSegments(names=[]):
     for name in names:
         vertexFile = name+".ver"
         edgeFile = name+".ari"
-        segments.append(readContentFromFiles(vertexFile, edgeFile))
+        found_edges = readContentFromFiles(vertexFile, edgeFile, points)
+        for edge in found_edges:
+            segments.append(edge)
+
+    return segments
 
 
-def readContentFromFiles(vertFile, edgeFile):
-    points = []
+def readContentFromFiles(vertFile, edgeFile, points=[]):
     edges = []
     try:
-        points = __readPoints__(vertFile)
+        __readPoints__(vertFile, points)
     except Exception as e:
         print(e)
 
-    print(points)
+    # print(points)  # delete
 
     try:
         edges = __convertToSegments__(edgeFile, points)
     except Exception as e:
         print(e)
+    return edges
 
 
-def __readPoints__(file):
-    points = []
+def __readPoints__(file, points):
     f = open(file, "r")
     if f.mode != "r":
         return Exception
@@ -42,7 +45,6 @@ def __readPoints__(file):
         x = int(data[1])
         y = int(data[2])
         points.append(Punto(x, y))
-    return points
 
 
 def __convertToSegments__(file, points):
@@ -52,7 +54,20 @@ def __convertToSegments__(file, points):
         return Exception
     content = f.readlines()
     content = content[4:]
+    origin_points = []
+    i = 1
     for line in content:
         data = " ".join(line.split())
         data = data.split(" ")
-        print(data)
+        origin = data[1]
+        origin = str(origin).replace("p", "")
+        origin = int(origin)
+        origin_points.append(origin)
+        i += 1
+        if i % 2 != 0:
+            PA = origin_points[0]
+            PB = origin_points[1]
+            edges.append(Segmento(points[PA-1], points[PB-1]))
+            origin_points.pop()
+            origin_points.pop()
+    return edges
--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from Reader import getSegments
 
 def main():
     names = ["ejemplo_01/layer01", "ejemplo_01/layer02"]
-    segmentos = getSegments(names)
+    points = []
+    segmentos = getSegments(names, points)
 
     # s1 = Segmento(Punto(10, 10), Punto(0, 0))
     # s2 = Segmento(Punto(10, 0), Punto(0, 10))
 
     # segmentos = [s1, s2]
 
-    # for s in segmentos:
-    #     print(s)
+    for s in segmentos:
+        print(s)
 
-    # barr = AlgoritmoBarrido(segmentos)
-    # barr.barrer()
-    # print(barr.R)
+    barr = AlgoritmoBarrido(segmentos)
+    barr.barrer()
+    print(barr.R)
 
 
 if __name__ == "__main__":
