5955ed03f3dddea9232e76fc86986b9001135731
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-03-23 13:51:37 -0600

    Clean

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ def main():
     points = []
     segmentos = getSegments(names, points)
 
-    # s1 = Segmento(Punto(10, 10), Punto(0, 0))
-    # s2 = Segmento(Punto(10, 0), Punto(0, 10))
-
-    # segmentos = [s1, s2]
-
     for s in segmentos:
         print(s)
 
